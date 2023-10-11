2e1efd179acbc921ec4153997cbd99515528762e
AndreeaGabrian <93679566+AndreeaGabrian@users.noreply.github.com>
2023-10-11 15:03:07 +0300

    Delete Limbaje formale si tehnici de compilare/Lab2/Lab2 directory

--- Limbaje formale si tehnici de compilare/Lab2/Lab2/test.py
+++
@@ -1,7 +0,0 @@
-import re
-
-#token='"multiplu3"'
-token='"nuEsteMultiplu_3"'
-result = re.search('^[0-9]$|^\'[0-9]\'$|^[1-9]+[0-9]*$|^\"[1-9]+[0-9]+\"$|^\'[A-Za-z]\'$|^\"[A-Za-z]+\w*\"$', token)
-print(len(token))
-print(result)
